33567a8963c6b12d7f1bfe658b8eb5385bee24fb
fionchadd <hello@cogdiss.com>
2020-03-11 13:26:25 +0000

    gallery

--- js/terrys.js
+++ js/terrys.js
@@ -1,6 +1,9 @@
-	    //slider for images
+jQuery(window).load(function() {
+//slider for images
         jQuery('.flexslider.gallery').flexslider({
             animation: "fade",
            slideshow: true,
               animationSpeed: 1000
           });
+        });
+          

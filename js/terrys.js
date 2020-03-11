31d5fa51a5d20953d264a3cdde1237b86efe6e4d
fionchadd <hello@cogdiss.com>
2020-03-11 14:34:29 +0000

    Update terrys.js

--- js/terrys.js
+++ js/terrys.js
@@ -9,5 +9,15 @@ jQuery(window).load(function() {
               controlNav: false,
               directionNav: false,	
           });
+
+          // viewport height on mobile
+          function resetHeight(){
+            // reset the body height to that of the inner browser
+            document.body.style.height = window.innerHeight + "px";
+        }
+        // reset the height whenever the window's resized
+        window.addEventListener("resize", resetHeight);
+        // called to initially set the height.
+        resetHeight();
         });
           
